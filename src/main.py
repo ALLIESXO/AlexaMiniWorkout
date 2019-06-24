5f5c2dd0b9469eff9b78e31a9d55d79ec8de149a
Alexander Lisogorov <alexander.lisogorov@uni-ulm.de>
2019-06-24 23:27:23 +0200

    added speechCollection: giving multiple possibilities of sentences alexa may say

--- src/main.py
+++ src/main.py
@@ -1,15 +1,30 @@
 from workout_controller import WorkoutController
+from flask import Flask, render_template
+from flask_ask import Ask, request, context, statement, question, session, delegate
+import yaml
+import codecs
+import random
+
+app = Flask(__name__)
+ask = Ask(app, "/")
 
 
-def main():
-    # Nur zum testen
+@ask.launch
+def launched():
+    session.attributes['state'] = 'greeting'
+    return question(get_speech(session.attributes['state']))
 
-    wc = WorkoutController()
-    # workout = wc.get_workout_by_name("Forces of Nature")
-    # print(workout['workout'])
-    print(wc.get_workout_by_user(3,14,0,0))
-    print(wc.get_workout_by_alexa(1, 2))
+
+def get_speech(state):
+
+    with codecs.open('speechCollection.yaml', 'r', encoding='utf-8') as stream:
+        doc = yaml.load(stream)
+        speech_list = doc[state]
+        random.shuffle(speech_list)
+        alexa_speaks = speech_list[0]
+
+    return alexa_speaks
 
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
